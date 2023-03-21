d8c0e42c74b1ea82e0e20787b64d71fc0f8ff886
Artem <aholevko@maxilect.com>
2023-03-21 22:04:22 +0300

    change tests

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -10,6 +10,7 @@ import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SearchTests extends TestBase {
 
@@ -28,26 +29,34 @@ public class SearchTests extends TestBase {
 
     @Tag("android")
     @Test
-    void openTheLinkTest() {
-        step("click on the link on the main page", () -> {
-            $(id("org.wikipedia.alpha:id/horizontal_scroll_list_item_text")).click();
+    void CheckButtonLogIn() {
+
+        step("Click on the NavBar Menu button", () -> {
+            $(id("org.wikipedia.alpha:id/menu_overflow_button")).click();
         });
-        step("Verify content found", () ->
-                $(id("org.wikipedia.alpha:id/view_news_fullscreen_story_text"))
-                        .shouldHave(text(
-                                "At the Academy Awards, Everything Everywhere All at Once wins seven awards, including Best Picture and Best Actress for Michelle Yeoh (pictured).")));
+        step("Check button log in", () -> {
+            $(id("org.wikipedia.alpha:id/explore_overflow_account_name")).shouldHave(text("Log in to Wikipedia"));
+            $(id("org.wikipedia.alpha:id/explore_overflow_account_name")).click();
+        });
+
+        step("Check text on button at login form", () -> {
+            $(id("org.wikipedia.alpha:id/login_button")).shouldHave(text("Log in"));
+        });
     }
 
     @Test()
     @Tag("ios")
-    void iosCheckInputTest() {
-        step("Type search", () -> {
-            $(accessibilityId("Search Wikipedia")).click();
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("java");
+    void loginTestFlight (){
+        step("Click button", () ->{
+            $(id("Text Button")).click();
         });
-        step("Verify content", () -> {
-            $$(id("org.wikipedia.alpha:id/page_list_item_title"))
-                    .shouldHave(sizeGreaterThan(0));
+        step("Write email on text area", () ->{
+            $(id("Text Input")).click();
+            $(id("Text Input")).sendKeys("sard@gmail.com");
+            $(id("Text Input")).pressEnter();
+        });
+        step("Check result", () ->{
+            assertEquals("sard@gmail.com", $(id("Text Output")).getText());
         });
     }
 
