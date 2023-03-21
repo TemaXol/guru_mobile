5bd9b645675a23f616cbe62c33c660532d1e04ca
Artem <aholevko@maxilect.com>
2023-03-21 22:34:53 +0300

    change tests 2

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -31,33 +31,26 @@ public class SearchTests extends TestBase {
     @Test
     void CheckButtonLogIn() {
 
-        step("Click on the NavBar Menu button", () -> {
-            $(id("org.wikipedia.alpha:id/menu_overflow_button")).click();
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("google");
         });
-        step("Check button log in", () -> {
-            $(id("org.wikipedia.alpha:id/explore_overflow_account_name")).shouldHave(text("Log in to Wikipedia"));
-            $(id("org.wikipedia.alpha:id/explore_overflow_account_name")).click();
-        });
-
-        step("Check text on button at login form", () -> {
-            $(id("org.wikipedia.alpha:id/login_button")).shouldHave(text("Log in"));
+        step("Verify content", () -> {
+            $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                    .shouldHave(sizeGreaterThan(0));
         });
     }
 
     @Test()
     @Tag("ios")
     void loginTestFlight (){
-        step("Click button", () ->{
-            $(id("Text Button")).click();
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("dodo pizza");
         });
-        step("Write email on text area", () ->{
-            $(id("Text Input")).click();
-            $(id("Text Input")).sendKeys("sard@gmail.com");
-            $(id("Text Input")).pressEnter();
+        step("Verify content", () -> {
+            $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                    .shouldHave(sizeGreaterThan(0));
         });
-        step("Check result", () ->{
-            assertEquals("sard@gmail.com", $(id("Text Output")).getText());
-        });
-    }
 
 }
